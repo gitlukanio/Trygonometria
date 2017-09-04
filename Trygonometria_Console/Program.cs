013b3046b16c74877e71a62d34a514074a0dcb52
Łukasz Łuczko <lukasz.luczko@gmail.com>
2017-09-04 13:48:25 +0200

    adsdd

--- Trygonometria_Console/Program.cs
+++ Trygonometria_Console/Program.cs
@@ -11,98 +11,36 @@ namespace Trygonometria_Console
     {
         static void Main(string[] args)
         {
-            //Point p1 = new Point(, );
-
-            Line l1 = new Line(new Point(1.0, 2.0), new Point(6.0, 4.0) );
-            Console.WriteLine("A: " + l1.A);
-            Console.WriteLine("B: " + l1.B);
-            
-            Console.WriteLine(l1.ToString());
-
-            Console.WriteLine("Dla X=1 Y=" + l1.Get_Y(1.0));
-            Console.WriteLine("Dla Y=4 X=" + l1.Get_X(4.0));
-
-            if (l1.IfPointBelongToLine(new Point(4, 3)))
-            {
-                Console.WriteLine("Punkt (3,4) lezy na lini" );
-            }
-            else
-            {
-                Console.WriteLine("Punkt (3,4) nie lezy na lini");
-            }
-
-            Console.WriteLine("======================================");
-
-            Line l2 = new Line(new Point(1.0, 1.0), new Point(6.0, 3.0)); // równoległa
-            Line l3 = new Line(new Point(2.0, 4.0), new Point(5.0, 2.0)); // przecięcie w punkcie 3,5 3
-
-            Console.WriteLine("Linia 1: " + l1.ToString());
-            Console.WriteLine("Linia 2: " + l3.ToString());
-
-            Point p3 = Line.PointOfIntersect(l1, l3);
-
-            if (p3 != null)
-            {
-                Console.WriteLine("Punk przeciecia: X={0} Y={1}", p3.X, p3.Y);
-            }
-            else
-            {
-                Console.WriteLine("Dwie linie się nie przecinają!!!");
-            }
-
-
-            Console.WriteLine("======================================");
-
-            Console.WriteLine("=====Generujemy linie prostopadłą=====");
-
-            Line l5 = new Line(new Point(1.0, 2.0), new Point(6.0, 4.0));
-
-            Line l6 = Line.GetPerpendicularLine(l5,new Point(6, 4));
-
-            if (Line.PointOfIntersect(l5, l6) != null)
-            {
-                Console.WriteLine("Punkt przecięcia: {0}\n\n", Line.PointOfIntersect(l5, l6).ToString());
-            }
-
-
-            Console.WriteLine("=====Odległośc punktów od siebie=====");
-            Point p1 = new Point(1,1);
-            Point p2 = new Point(2.5, 0.5);
-            Console.WriteLine("Punkt1 = {0}, Punkt2 = {1}", p1.ToString(), p2.ToString());
-            Console.WriteLine("Odległość = {0}\n\n", Point.DistanceBetweenTwoPoints(p1, p2));
-
-            Console.WriteLine("\n\n=================================================================");
-            Console.WriteLine("Teraz trójkąt");
-            Point tra = new Point(1, 1);
-            Point trb = new Point(2, 2.5);
-            Point trc = new Point(4, 1);
-            Triangle tr = new Triangle(tra, trb, trc);
-
-            Console.WriteLine("Obwód trójkąta: {0}", tr.GetCircumference());
-            Console.WriteLine("Pole trójkąta: {0}\n\n", tr.GetArea());
-            //onsole.WriteLine("CB:{0}", tr.SectorBC.Line.ToString());
-
-            Console.WriteLine(tr.ToString());
-
-            Console.WriteLine("\n\n=================================================================");
-            Console.WriteLine("Teraz koło");
-            Circle k = new Circle(tra, 2);
-
-            Console.WriteLine(k.ToString());
-
             Console.WriteLine("\n\n=================================================================");
             Console.WriteLine("Test");
+            
+            Point a = new Point(3,1);
+            Point b = new Point(3,4);
+            Point c = new Point(7,1);
+            //Point c = new Point(10, 1);
 
-            Sector s = new Sector(new Point(3,4), new Point(7,1) );
-
-            Console.WriteLine("Sektor długość: {0}", s.Lenght);
-
-
-
-
-
+            Triangle tr = new Triangle(a,b,c);
 
 
+            //Sector s = new Sector(new Point(3,4), new Point(7,1) );
+            //Console.WriteLine("Sektor długość: {0}", s.Lenght);
+
+            Console.WriteLine("Czy jest prostokątny: {0}", tr.IsTriangleRectangular());
+            Console.WriteLine("Promien wpisanego: {0}", tr.PromienOkreguWpisanego());
+
+            double r = tr.PromienOkreguWpisanego();
+            Point pac = new Point(tr.A.X+r*2, tr.SectorAC.Line.Get_Y(tr.A.X + r*2));
+            Line LPC = Line.GetPerpendicularLine(tr.SectorAC.Line, pac);
+            //Console.WriteLine("Punkt PAC:{0}", pac.ToString());
+            Point pab = new Point(tr.SectorAB.Line.Get_X(tr.A.Y + r * 2),tr.A.Y+r*2);
+            Line LPB = Line.GetPerpendicularLine(tr.SectorAB.Line, pab);
+
+            Point paaaaa = Line.PointOfIntersect(LPC, LPB);
+            Console.WriteLine("Punkt PAAAA{0}", paaaaa.ToString());
+
+            
+
+            
 
             //Console.ReadKey();
 
--- TrygonometryLibrary/Triangle.cs
+++ TrygonometryLibrary/Triangle.cs
@@ -9,25 +9,29 @@ namespace TrygonometryLibrary
         public Point B { get; }
 
         public Point C { get; }
-        //=======================================================
-        //1
-        public Sector SectorAB
-        {
-            get
-            {
-                return new Sector(A, B);
-            }
-        }
+        ////=======================================================
+        ////1
+        //public Sector SectorAB
+        //{
+        //    get
+        //    {
+        //        return new Sector(A, B);
+        //    }
+        //}
 
-        //2
-        public Sector SectorAC()
-        {
-            return new Sector(A, C);
-        }
+        ////2
+        //public Sector SectorAC()
+        //{
+        //    return new Sector(A, C);
+        //}
 
-        //3
+        ////3
+        //public Sector SectorBC => new Sector(B, C);
+        ////========================================================
+
+        public Sector SectorAB => new Sector(A, B);
+        public Sector SectorAC => new Sector(A, C);
         public Sector SectorBC => new Sector(B, C);
-        //========================================================
 
 
         public Triangle(Point a, Point b, Point c)
@@ -41,17 +45,16 @@ namespace TrygonometryLibrary
         {
             return "Trójkąt - A(" + A.ToString() + "), B(" + B.ToString() + "), C(" + C.ToString() + ")\n" +
                    "AB:(" + SectorAB.ToString() + ")\n" +
-                   "AC:("+SectorAC().ToString()+")\n" +
+                   "AC:("+SectorAC.ToString()+")\n" +
                    "CB:("+SectorBC.ToString()+")\n" +
                    "Pole:("+this.GetArea()+"), " + 
                    "Obwód:("+this.GetCircumference()+")";
-
         }
 
 
         public double GetCircumference()
         {
-            return SectorAB.Lenght + SectorBC.Lenght + SectorAC().Lenght;
+            return SectorAB.Lenght + SectorBC.Lenght + SectorAC.Lenght;
         }
 
         public double GetArea()
@@ -59,7 +62,39 @@ namespace TrygonometryLibrary
             Line hLine = Line.GetPerpendicularLine(SectorBC.Line, A);
             Point hPoint = Line.PointOfIntersect(SectorBC.Line, hLine);
             return 0.5 * SectorBC.Lenght * Point.DistanceBetweenTwoPoints(A, hPoint);
-
         }
+
+        public Point GetCenterOfGravity()
+        {
+            double x = (this.A.X + this.B.X + this.C.X) / 3;
+            double y = (this.A.Y + this.B.Y + this.C.Y) / 3;
+            return new Point(x, y);
+        }
+
+        public bool IsTriangleRectangular()
+        {
+            double AB = Math.Pow(SectorAB.Lenght,2);
+            double AC = Math.Pow(SectorAC.Lenght,2);
+            double BC = Math.Pow(SectorBC.Lenght,2);
+
+            if (AB+AC==BC || AC+BC==AB || AB+BC==AC)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+            
+        }
+
+        public double PromienOkreguWpisanego()
+        {
+            return 2 * this.GetArea() / (SectorAC.Lenght + SectorAB.Lenght + SectorBC.Lenght);
+        }
+
+
+
+
     }
 }
